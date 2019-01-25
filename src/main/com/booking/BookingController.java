ca2622c170425072848d4bd186a3a0e3eaa7aa1b
Valentyna Lysenok <tina.lysenok@gmail.com>
2019-01-25 18:44:00 +0200

    y

--- src/main/com/booking/BookingController.java
+++ src/main/com/booking/BookingController.java
@@ -6,16 +6,16 @@ public class BookingController {
 
     private BookingsService bookingsService;
 
-    public Booking createBooking(Flight flight, String name, String surname) {
-        return bookingsService.createBooking(flight, name, surname);
+    public Booking createBooking(Flight flight, String name, String surname, FlightController flightController) {
+        return bookingsService.createBooking(flight, name, surname, flightController);
     }
 
     public boolean deleteBookingByID(int ID) {
         return bookingsService.deleteBookingByID(ID);
     }
 
-    public List<Booking> showSelectedBookings(String name, String surname, int ID) {
-        return bookingsService.showSelectedBookings(name, surname, ID);
+    public List<Booking> showSelectedBookings(String name, String surname) {
+        return bookingsService.showSelectedBookings(name, surname);
     }
 
 }
